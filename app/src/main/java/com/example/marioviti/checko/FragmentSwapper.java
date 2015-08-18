1f90f4caab2538e1f2fba79b8aa4c7351d5b3c21
marioviti <vitimario2@gmail.com>
2015-08-18 13:43:01 +0200

    PageFragment da fare

---
+++ app/src/main/java/com/example/marioviti/checko/FragmentSwapper.java
@@ -0,0 +1,9 @@
+package com.example.marioviti.checko;
+
+/**
+ * Created by marioviti on 18/08/15.
+ */
+public interface FragmentSwapper {
+
+    public boolean swapWith(int pos);
+}
---
+++ app/src/main/java/com/example/marioviti/checko/RootFragment.java
@@ -0,0 +1,57 @@
+package com.example.marioviti.checko;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by marioviti on 18/08/15.
+ */
+public class RootFragment extends Fragment implements FragmentSwapper {
+
+    private FragmentPool fgtPool;
+    private int PAG_NUM = 3;
+
+    @Override
+    public View onCreateView (LayoutInflater li, ViewGroup container, Bundle si) {
+
+        fgtPool = new FragmentPool(PAG_NUM);
+        for(int i = 0; i<PAG_NUM; i++) {
+            fgtPool.inesertFragment(PageFragment.newInstance("page",i));
+        }
+
+    }
+
+    @Override
+    public boolean swapWith (int pos) {
+        return false;
+    }
+
+    private class FragmentPool {
+        private int curr;
+        private int length;
+        private Fragment[] fragArray;
+
+        public FragmentPool (int lenght) {
+            fragArray = new Fragment[lenght];
+            this.length = lenght;
+            curr = 0;
+        }
+
+        public boolean inesertFragment (Fragment f) {
+            if(curr==length)
+                return false;
+            fragArray[curr]=f;
+            curr++;
+            return true;
+        }
+
+        public Fragment getAt (int i) {
+            if(i>=0 && i<length)
+                return fragArray[i];
+            return null;
+        }
+    }
+}
